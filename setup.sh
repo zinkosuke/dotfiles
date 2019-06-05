ce5127e5b6de16409e5b79236ffc7db80ef5ad76
zinkosuke <kosuke.jin@outlook.jp>
2019-06-05 17:05:59 +0900

    MOD | zshrc add alias

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# https://brew.sh/index_ja
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install \
+    awk \
+    ghq \
+    git \
+    jq \
+    vim \
+    peco \
+    tmux \
+    tree \
+    zsh
+
 CD=$(cd $(dirname $0); pwd)
 
 for fn in $(ls -1aF | grep '^\.' | grep -v '\/$' | grep -v swp); do
